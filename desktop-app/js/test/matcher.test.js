0b89675bc22fb252f249b952baa67a4b0036d72c
Sanjeyac <sanjeya.cooray@gmail.com>
2018-10-18 06:38:33 +0200

    added tests folder

---
+++ desktop-app/js/test/matcher.test.js
@@ -0,0 +1,10 @@
+const Matcher = require('../models/matcher.js');
+const DataInstant = require('../models/datainstant.js');
+
+test('matcher with regex "firefox" on instant "Mozilla Firefox"', () => {
+
+    let instant = new DataInstant('Mozilla Firefox', 1);
+    let matcher = new Matcher('Firefox','firefox');
+
+    expect(matcher.match(instant)).toBe(true);
+});
